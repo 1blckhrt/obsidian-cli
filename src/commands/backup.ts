2e0e205c7cca78aa169fad6ed43012d0afa43f1e
1blckhrt <williams.1691@wright.edu>
2025-03-31 11:57:02 -0400

    feat: backup command

--- src/commands/backup.ts
+++ src/commands/backup.ts
@@ -1,3 +1,23 @@
-export default function backupVault(backupPath: string) {
+import { promises as fs } from 'node:fs';
+import { getVaultPath } from '../util/helpers.js';
+
+export default async function backupVault(backupPath: string) {
   console.log(`Backing up vault to ${backupPath}`);
+
+  try {
+    await fs.access(backupPath);
+    console.log('Found backup directory!');
+  } catch {
+    console.log('Backup directory not found, creating...');
+    await fs.mkdir(backupPath, { recursive: true });
+    console.log('Backup directory created!');
+  }
+
+  try {
+    const vaultPath = await getVaultPath();
+    await fs.cp(vaultPath, backupPath, { recursive: true });
+    console.log('The backup was successful!');
+  } catch (error) {
+    console.error('Failed to backup vault!', error);
+  }
 }
